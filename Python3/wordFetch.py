60ac2492acf37fbb2ad8ec99de5b512a33fa41d5
JakeRyanPlatt <jakeplatt176290@gmail.com>
2025-12-16 09:14:14 -0500

    Refactor to allow multiple URLs and added function definitions for sorting

--- Python3/wordFetch.py
+++ Python3/wordFetch.py
@@ -2,7 +2,7 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-PAGE_URL = 'http://94.237.61.249:34866'
+PAGE_URL = 'http://94.237.61.249:53049'
 
 def get_html_of(url):
     resp = requests.get(url)
@@ -12,23 +12,32 @@ def get_html_of(url):
         exit(1)
 
     return resp.content.decode()
-    
 
-html = get_html_of(PAGE_URL)
-soup = BeautifulSoup(html, 'html.parser')
-raw_text = soup.get_text()
-all_words = re.findall(r'\w+', raw_text)
+def count_occurences_in(word_lists):
+    word_count = {}
 
-word_count = {}
-
-for word in all_words:
+    for word in all_words:
         if word not in word_count:
             word_count[word] = 1
         else:
             current_count = word_count.get(word)
             word_count[word] = current_count + 1
+    return word_count
 
-top_words = sorted(word_count.items(),key=lambda item: item[1], reverse=True)
+
+def get_all_words_from(url):
+    html = get_html_of(PAGE_URL)
+    soup = BeautifulSoup(html, 'html.parser')
+    raw_text = soup.get_text()
+    return re.findall(r'\w+', raw_text)
+
+def get_top_words_from(url):
+    all_words = get_all_words_from(url)
+    occurences = count_occurences_in(all_words)
+    return sorted(occurences.items(), key=lambda item: item[1], reverse=True)
+
+all_words = get_all_words_from(PAGE_URL)
+top_words = get_top_words_from(all_words)
 
 for i in range(10):
     print(top_words[i][0])
