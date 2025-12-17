f4c2c8f0a5bca2fcf987094b67b21ee4f76372e2
JakeRyanPlatt <jakeplatt176290@gmail.com>
2025-12-17 15:48:30 -0500

    My first C# App

---
+++ C#/MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace MyNamespace
+{
+    class MyClass {} 
+
+    class Program
+    {
+        static void Main()
+        {
+            // Using fully qualified name to avoid naming conflict
+            MyNamespace.MyClass myObject = new MyNamespace.MyClass();
+        }
+    }
+}
